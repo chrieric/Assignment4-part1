0d7f8f7e586770cfc68e1c1acf9a613e74e87d5b
chrieric <chrieric@onid.oregonstate.edu>
2015-02-05 21:03:12 -0600

    Mult 1
    Initial attempt at creating multtable. May need some more organizational
    work. Most likely will need to figure out if the $_GET array keeps it's
    key values when passed by value.

--- src/multtable.php
+++ src/multtable.php
@@ -1,75 +1,88 @@
+<?php
+error_reporting(E_ALL);
 ini_set('display_errors',1);
+	
+function dataValidation($array)
+{
+	$var_b = true;
+			
+	foreach($array is $k => $v)
+	{
+		if($v != null)
+		{
+			if(bool is_int($v) != True)
+			{
+				var_b = False;
+				echo $k . ' Must be an integer';
+			}
+		}
+		else
+		{
+			var_b = False;
+			echo 'Missing parameter ' . $k;
+		}
+	}
+	
+	return var_b;
+};
+			
+function isLessThan($array)
+{
+	$mincand = $array[0];
+	$maxcand = $array[1];
+	$minplier = $array[2];
+	$maxplier = $array[3];
+			
+	if($mincand > $maxcand)
+	{
+		echo 'Minimum multiplicand is greater than maximum';
+	}
+	else if($minplier > $maxplier)
+	{
+		echo 'Minimum multiplier is greater than maximum multiplier';
+	}
+	else
+	{
+		return True;
+	}
+};
+
+function createTable($array)
+{
+	$height = $array[1] - $array[0];
+	$width = $array[3] - $array[2];
+	$max_h = $height;
+	$max_w = $width;
+	
+	while($height > 0)
+	{
+		echo '<tr>';
+		while($width > 0)
+		{
+			echo '<td>($max_w-$width)*($max_h-$height)</td>';
+			$width-=1;
+		}
+		echo </tr>;
+	}
+}
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<meta charset="utf-8"/>
 <head>
-<meta charset='utf-8'>
-<title>Multtable</title>
+	<title>Multiplicand Table</title>
 </head>
 <body>
 <?php
-error_reporting(E_ALL);
-	<section>
-		<header>
-		<h3>Placeholder</h3>
-		</header>
-		
-		function dataValidation($array)
-		{
-			$var_b = true;
-			
-			foreach($array is $k => $v)
-			{
-				if($v != null)
-				{
-					if(bool is_int($v) != true)
-					{
-					
-					}
-					else
-					{
-						'Must be an integer';
-					}
-				}
-				else
-				{
-					echo 'Missing parameter';
-				}
-			}
-		}
-		
-		
-		
-		function isLessThan($array)
-		{
-			$mincand = $array[0];
-			$maxcand = $array[1];
-			$minplier = $array[2];
-			$maxplier = $array[3];
-			
-			if($mincand > $maxcand)
-			{
-				echo 'Minimum multiplicand is greater than maximum';
-			}
-			else if($minplier > $maxplier)
-			{
-				echo 'Minimum multiplier is greater than maximum pultiplier';
-			}
-			else
-			{
-				
-			}
-			
-		}
-	
-		<form action='http://web.engr.oregonstate.edu/~chrierc/multtable.php/multtable.php' method='GET'>
-		<input type='text' name = 'min-multiplicand' value='Minimum Multiplicand'>
-		<input type = 'text' name = 'max-multiplicand' value = 'Maximum Multiplicand'>
-		<input type = 'text' name = 'min-multiplier' value = 'Minimum Multiplier'>
-		<input type = 'text' name = 'max-multiplier' value = 'Maximum Multiplier'>
-		</form>
-		
-		
-	</section
+
+if(dataValidation($_GET))
+{
+	if(isLessThan($_GET))
+	{
+		createTable($_GET);
+	}
+}
+?>
 </body>
 </html>
-?>
